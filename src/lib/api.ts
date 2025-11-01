710e33bd7985befb9ee0894519c051add3432911
Laura <laurabarros2541@gmail.com>
2025-11-01 01:45:44 -0300

    add ajustes deploy

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: '/api', // todas as requisições serão redirecionadas para /.netlify/functions/proxy/
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json'
   },
 });
 
